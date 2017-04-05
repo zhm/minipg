468be82d3a8a92455a31f34a9ba0548d0fc5c288
Zac McCormick <zac.mccormick@gmail.com>
2017-04-05 14:00:05 -0400

    Add done param to allow async continuation of each loop

--- src/cursor.js
+++ src/cursor.js
@@ -12,32 +12,35 @@ export default class Cursor {
 
   each(callback) {
     this.next((err, {finished, columns, values, index, client}) => {
-      /* eslint-disable callback-return */
-      callback(err, {finished, columns, values, index, client});
-      /* eslint-enable callback-return */
+      const done = () => {
+        if (!finished) {
+          this.each(callback);
+        }
+      };
 
-      if (!finished) {
-        this.each(callback);
-      }
+      callback(err, {finished, columns, values, index, client, done});
     });
   }
 
   eachBatch(callback) {
     this.nextBatch(() => {
+      const done = () => {
+        this.index += this.batch.length;
+
+        if (!this.finished) {
+          this.eachBatch(callback);
+        }
+      };
+
       /* eslint-disable callback-return */
       callback(this.error,
                {finished: this.finished,
                 columns: this.columns,
                 values: this.batch,
                 index: this.index,
-                client: this.client});
+                client: this.client,
+                done});
       /* eslint-enable callback-return */
-
-      this.index += this.batch.length;
-
-      if (!this.finished) {
-        this.eachBatch(callback);
-      }
     });
   }
 
