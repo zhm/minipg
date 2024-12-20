8c88e88eeae3dcd6f84dc83fcc8c3cbddd3cc3f5
Zac McCormick <zac.mccormick@gmail.com>
2024-12-20 12:21:16 -0500

    Update deps

--- dist/cursor.js
+++ dist/cursor.js
@@ -4,17 +4,15 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports["default"] = void 0;
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
-
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
-
-var Cursor = /*#__PURE__*/function () {
+function _typeof(o) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (o) { return typeof o; } : function (o) { return o && "function" == typeof Symbol && o.constructor === Symbol && o !== Symbol.prototype ? "symbol" : typeof o; }, _typeof(o); }
+function _classCallCheck(a, n) { if (!(a instanceof n)) throw new TypeError("Cannot call a class as a function"); }
+function _defineProperties(e, r) { for (var t = 0; t < r.length; t++) { var o = r[t]; o.enumerable = o.enumerable || !1, o.configurable = !0, "value" in o && (o.writable = !0), Object.defineProperty(e, _toPropertyKey(o.key), o); } }
+function _createClass(e, r, t) { return r && _defineProperties(e.prototype, r), t && _defineProperties(e, t), Object.defineProperty(e, "prototype", { writable: !1 }), e; }
+function _toPropertyKey(t) { var i = _toPrimitive(t, "string"); return "symbol" == _typeof(i) ? i : i + ""; }
+function _toPrimitive(t, r) { if ("object" != _typeof(t) || !t) return t; var e = t[Symbol.toPrimitive]; if (void 0 !== e) { var i = e.call(t, r || "default"); if ("object" != _typeof(i)) return i; throw new TypeError("@@toPrimitive must return a primitive value."); } return ("string" === r ? String : Number)(t); }
+var Cursor = exports["default"] = /*#__PURE__*/function () {
   function Cursor(client) {
     _classCallCheck(this, Cursor);
-
     this.client = client;
     this.batchOffset = 0;
     this.batchStart = 0;
@@ -24,25 +22,21 @@ var Cursor = /*#__PURE__*/function () {
     this.finished = false;
     this.needsMetadata = true;
   }
-
-  _createClass(Cursor, [{
+  return _createClass(Cursor, [{
     key: "each",
     value: function each(callback) {
       var _this = this;
-
       this.next(function (err, _ref) {
         var finished = _ref.finished,
-            columns = _ref.columns,
-            values = _ref.values,
-            index = _ref.index,
-            client = _ref.client;
-
+          columns = _ref.columns,
+          values = _ref.values,
+          index = _ref.index,
+          client = _ref.client;
         var done = function done() {
           if (!finished) {
             _this.each(callback);
           }
         };
-
         callback(err, {
           finished: finished,
           columns: columns,
@@ -57,18 +51,15 @@ var Cursor = /*#__PURE__*/function () {
     key: "eachBatch",
     value: function eachBatch(callback) {
       var _this2 = this;
-
       this.nextBatch(function () {
         var done = function done() {
           _this2.index += _this2.batch.length;
-
           if (!_this2.finished) {
             _this2.eachBatch(callback);
           }
         };
+
         /* eslint-disable callback-return */
-
-
         callback(_this2.error, {
           finished: _this2.finished,
           columns: _this2.columns,
@@ -84,20 +75,16 @@ var Cursor = /*#__PURE__*/function () {
     key: "next",
     value: function next(callback) {
       var _this3 = this;
-
       this._index = this._index != null ? this._index : 0;
-
       var processResult = function processResult() {
         var values = null;
-
         if (_this3.batch.length) {
           var row = _this3.batch[_this3.batchOffset];
           _this3.batchOffset += 1;
           values = row ? row.values : null;
         }
+
         /* eslint-disable callback-return */
-
-
         callback(_this3.error, {
           finished: _this3.finished && _this3.batchOffset === _this3.batch.length,
           columns: _this3.columns,
@@ -108,7 +95,6 @@ var Cursor = /*#__PURE__*/function () {
         _this3._index++;
         /* eslint-enable callback-return */
       };
-
       if (this.batchOffset < this.batch.length) {
         if (this.batchOffset % 1000 === 0) {
           process.nextTick(processResult);
@@ -124,28 +110,29 @@ var Cursor = /*#__PURE__*/function () {
     key: "nextBatch",
     value: function nextBatch(callback) {
       var _this4 = this;
-
       if (this.needsMetadata) {
         this.index = 0;
         this.columns = null;
       }
-
       this.client.getResults(this.needsMetadata, function (results) {
         _this4.needsMetadata = false;
         _this4.batch = results;
         _this4.batchOffset = 0;
         _this4.finished = _this4.client.nativeClient.finished();
         var hasResult = results && results.length;
-        var hasColumns = hasResult && results[0] && results[0].columns; // results == [ null ]
+        var hasColumns = hasResult && results[0] && results[0].columns;
 
-        var emptyResultSet = hasResult && results[results.length - 1] == null; // results == [ ..., {} ]
+        // results == [ null ]
+        var emptyResultSet = hasResult && results[results.length - 1] == null;
 
+        // results == [ ..., {} ]
         var endOfResultSet = hasResult && results[results.length - 1] && results[results.length - 1].values == null;
-
         if (hasColumns) {
           _this4.batchStart = 0;
           _this4.columns = results[0].columns;
-        } // There are several possible states here because the client supports
+        }
+
+        // There are several possible states here because the client supports
         // multiple result sets in a single query and the complexity that batching adds.
         //
         // finished?               -> we are done, don't do anything
@@ -157,36 +144,26 @@ var Cursor = /*#__PURE__*/function () {
         //                            that the next call to getResults will request the column metadata of the next query.
         //                            This is important when, for example, there are 2 completely different SELECT statements
         //                            in the command text. In that case we need to ask for metadata twice.
-
-
         if (!_this4.finished && (emptyResultSet || endOfResultSet)) {
           _this4.needsMetadata = true;
         }
-
         var error = _this4.client.lastError;
-
         if (error) {
           _this4.error = error;
         }
+
         /* eslint-disable callback-return */
-
-
         callback();
         /* eslint-enable callback-return */
 
         if (results) {
           _this4.batchStart += results.length;
         }
-
         if (endOfResultSet) {
           _this4.batchStart = 0;
         }
       });
     }
   }]);
-
-  return Cursor;
 }();
-
-exports["default"] = Cursor;
 //# sourceMappingURL=cursor.js.map
--- dist/index.js
+++ dist/index.js
@@ -3,49 +3,37 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.createPool = createPool;
 exports.Client = void 0;
-
+exports.createPool = createPool;
 var _cursor = _interopRequireDefault(require("./cursor"));
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
-
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
-
+function _interopRequireDefault(e) { return e && e.__esModule ? e : { "default": e }; }
+function _typeof(o) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (o) { return typeof o; } : function (o) { return o && "function" == typeof Symbol && o.constructor === Symbol && o !== Symbol.prototype ? "symbol" : typeof o; }, _typeof(o); }
+function _classCallCheck(a, n) { if (!(a instanceof n)) throw new TypeError("Cannot call a class as a function"); }
+function _defineProperties(e, r) { for (var t = 0; t < r.length; t++) { var o = r[t]; o.enumerable = o.enumerable || !1, o.configurable = !0, "value" in o && (o.writable = !0), Object.defineProperty(e, _toPropertyKey(o.key), o); } }
+function _createClass(e, r, t) { return r && _defineProperties(e.prototype, r), t && _defineProperties(e, t), Object.defineProperty(e, "prototype", { writable: !1 }), e; }
+function _toPropertyKey(t) { var i = _toPrimitive(t, "string"); return "symbol" == _typeof(i) ? i : i + ""; }
+function _toPrimitive(t, r) { if ("object" != _typeof(t) || !t) return t; var e = t[Symbol.toPrimitive]; if (void 0 !== e) { var i = e.call(t, r || "default"); if ("object" != _typeof(i)) return i; throw new TypeError("@@toPrimitive must return a primitive value."); } return ("string" === r ? String : Number)(t); }
 var NativeClient = require('bindings')('addon').Client;
-
 var genericPool = require('generic-pool');
-
 function defaultNoticeProcessor(message) {
   console.warn(message);
 }
-
 var nextClientID = 0;
-
-var Client = /*#__PURE__*/function () {
+var Client = exports.Client = /*#__PURE__*/function () {
   function Client() {
     _classCallCheck(this, Client);
-
     this.nativeClient = new NativeClient();
     this.id = ++nextClientID;
   }
-
-  _createClass(Client, [{
+  return _createClass(Client, [{
     key: "connect",
     value: function connect(string, callback) {
       var _this = this;
-
       this.nativeClient.connect(string, function (err) {
         if (err) {
           return callback(err, _this);
         }
-
         _this.nativeClient.setNoticeProcessor(Client.defaultNoticeProcessor || defaultNoticeProcessor);
-
         return callback(null, _this);
       });
     }
@@ -55,16 +43,15 @@ var Client = /*#__PURE__*/function () {
       if (!this.nativeClient.finished()) {
         throw new Error('client in use', this.id);
       }
-
       this.nativeClient.query(sql);
       return new _cursor["default"](this);
-    } // fetch a single result record
+    }
 
+    // fetch a single result record
   }, {
     key: "getResult",
     value: function getResult(returnMetadata, callback) {
       var _this2 = this;
-
       Client.setImmediate(function () {
         callback(_this2.nativeClient.getResult(returnMetadata));
       });
@@ -73,7 +60,6 @@ var Client = /*#__PURE__*/function () {
     key: "getResults",
     value: function getResults(returnMetadata, callback) {
       var _this3 = this;
-
       Client.setImmediate(function () {
         callback(_this3.nativeClient.getResults(returnMetadata));
       });
@@ -92,30 +78,21 @@ var Client = /*#__PURE__*/function () {
     key: "lastError",
     get: function get() {
       var error = this.nativeClient.lastError();
-
       if (error == null) {
         return null;
       }
-
       var queryError = new Error();
-
       for (var prop in error) {
         if (error.hasOwnProperty(prop)) {
           queryError[prop] = error[prop];
         }
       }
-
       return queryError;
     }
   }]);
-
-  return Client;
 }();
-
-exports.Client = Client;
 Client.setImmediate = setImmediate;
 Client.defaultNoticeProcessor = defaultNoticeProcessor;
-
 function createPool(options) {
   /* eslint-disable new-cap */
   return genericPool.Pool({
@@ -125,7 +102,6 @@ function createPool(options) {
         if (err) {
           return callback(client ? client.lastError || err : err);
         }
-
         return callback(null, client);
       });
     },
